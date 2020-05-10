ce196d7060bbcfede9439be420623e9f1c0836cc
Robert Harty <44852780+Bobbytruth@users.noreply.github.com>
2020-05-10 16:12:08 +0100

    Add files via upload

---
+++ DNSAUDIT.ps1
@@ -0,0 +1,16 @@
+﻿#Run 'Powershell -Command "c:\temp\DNSaudit.ps1" > c:\temp\dnsout.txt 2>&1'
+CLS
+$servers = Get-Content "c:\temp\servers.txt" 
+
+foreach($server in $servers){
+$Adapters = Get-WmiObject -ComputerName $server Win32_NetworkAdapterConfiguration
+
+	Foreach ($Adapter in ($Adapters | Where {$_.IPEnabled -eq $True})) {
+	$Details = "" | Select Description, "Physical address", "IP Address / Subnet Mask", "Default Gateway", "DHCP Enabled", DNS, WINS
+		If ($Adapter.DNSServerSearchOrder -ne $Null)	{
+			$Details.DNS =  "$($Adapter.DNSServerSearchOrder)"
+			Write-Host $server      -ForegroundColor DarkBlue $Details.DNS
+					}
+					
+	}
+}
